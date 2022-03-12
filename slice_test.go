59f5bc2bc572a1c3ad43800af7c0029868cf4ac0
Kristian Bolino <kbolino@kbolino.com>
2022-03-11 20:17:34 -0500

    Add regression test for broken fakeByteSlice

---
+++ slice_test.go
@@ -0,0 +1,19 @@
+package nk
+
+import (
+	"testing"
+)
+
+const (
+	length = 5
+)
+
+// TestFakeByteSlice is a regression test for an issue where fakeByteSlice
+// was not setting the pointer of the slice correctly.
+func TestFakeByteSlice(t *testing.T) {
+	var b [length]byte
+	slice := fakeByteSlice(&b[0], length)
+	if &b[0] != &slice[0] {
+		t.Fatal("slice unequal")
+	}
+}
